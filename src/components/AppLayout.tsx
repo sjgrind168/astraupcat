754814765f28f85e386ee5c534323a914a50f4c4
sjgrind168 <sjgrind.ai@gmail.com>
2026-05-25 15:11:36 +0800

    move upgrade button to header

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "./AppSidebar";
 import { useApp } from "@/lib/store";
